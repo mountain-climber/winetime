018211974568dd84a8afd354959f1fa1ead282bb
mountain-climber <doublem.jquery@gmail.com>
2021-09-10 22:57:54 +0300

    added contact and adp for this

--- app/js/main.js
+++ app/js/main.js
@@ -59,6 +59,14 @@ $(function () {
     });
   });
 
+  $(".star").rateYo({
+    starWidth: "30px",
+    normalFill: "transparent",
+    ratedFill: "#EABC78",
+    readOnly: true,
+    starSvg: '<svg width="30px" height="30px" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 1.61804L17.8922 10.5193L18.0044 10.8647H18.3677H27.727L20.1552 16.366L19.8613 16.5795L19.9735 16.925L22.8657 25.8262L15.2939 20.325L15 20.1115L14.7061 20.325L7.13428 25.8262L10.0265 16.925L10.1387 16.5795L9.84482 16.366L2.27299 10.8647H11.6323H11.9956L12.1078 10.5193L15 1.61804Z" stroke="#EABC78"/></svg>'
+  });
+
   function hoverlink() {
     document.querySelector('.menu__list-link--catalogue').classList.add('menu__list-link--catalogue-open');
     document.querySelector('.menu__list-link-box').classList.add('menu__list-link-box--open');
@@ -149,14 +157,6 @@ $(function () {
         var num = $('input.product__form-num').attr('value', min);
       }
     }
-  });
-
-  $(".star").rateYo({
-    starWidth: "30px",
-    normalFill: "transparent",
-    ratedFill: "#EABC78",
-    readOnly: true,
-    starSvg: '<svg width="30px" height="30px" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 1.61804L17.8922 10.5193L18.0044 10.8647H18.3677H27.727L20.1552 16.366L19.8613 16.5795L19.9735 16.925L22.8657 25.8262L15.2939 20.325L15 20.1115L14.7061 20.325L7.13428 25.8262L10.0265 16.925L10.1387 16.5795L9.84482 16.366L2.27299 10.8647H11.6323H11.9956L12.1078 10.5193L15 1.61804Z" stroke="#EABC78"/></svg>'
   });
 
 
